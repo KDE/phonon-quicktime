debf4ffc2819e04272f7f2e1c379185a3e3812e9
Matthias Kretz <kretz@kde.org>
2006-05-03 19:09:49 +0000

    remove fake backend stuff

--- audiooutput.cpp
+++ audiooutput.cpp
@@ -33,7 +33,6 @@ AudioOutput::AudioOutput( QObject* parent, XineEngine* xe )
 	: AbstractAudioOutput( parent )
 	, m_xine_engine( xe )
 	, m_device( 1 )
-	, m_dsp( "/dev/dsp" )
 {
 }
 
@@ -79,76 +78,6 @@ void AudioOutput::setOutputDevice( int newDevice )
 	Q_ASSERT( newDevice >= 1 );
 	Q_ASSERT( newDevice <= 2 );
 	m_device = newDevice;
-}
-
-void AudioOutput::processBuffer( const QVector<float>& buffer )
-{
-	//static QFile indump( "indump" );
-	//if( !indump.isOpen() )
-		//indump.open( QIODevice::WriteOnly );
-	//static QFile outdump( "outdump" );
-	//if( !outdump.isOpen() )
-		//outdump.open( QIODevice::WriteOnly );
-	/*openDevice();
-	if( !m_dsp.isOpen() )
-		return;
-
-	// we dump the data in /dev/dsp
-	qint16* pcm = new qint16[ 2*buffer.size() ]; // 2* for stereo
-	char* towrite = reinterpret_cast<char*>( pcm );
-	int converted;
-	for( int i = 0; i < buffer.size(); ++i )
-	{
-		//indump.write( QByteArray::number( buffer[ i ] ) + "\n" );
-		converted = static_cast<qint16>( m_volume * buffer[ i ] * static_cast<float>( 0x7FFF ) );
-		//outdump.write( QByteArray::number( converted ) + "\n" );
-		*pcm++ = converted;
-		*pcm++ = converted;
-	}
-	int size = sizeof( qint16 ) * 2 * buffer.size();
-	int written;
-	while( size > 0 )
-	{
-		written = m_dsp.write( towrite, size );
-		// QFSFileEngine loops until errno != EINTR
-		if( written < 0 )
-			break;
-		size = size - written;
-		if( size > 0 )
-		{
-			towrite += written;
-			kWarning() << "only " << written << " bytes written to /dev/dsp" << endl;
-		}
-	}
-
-	pcm -= 2*buffer.size();
-	delete[] pcm;*/
-}
-
-void AudioOutput::openDevice()
-{
-	/*if( m_dsp.isOpen() )
-		return;
-
-#ifdef HAVE_SYS_SOUNDCARD_H
-	if( !m_dsp.open( QIODevice::WriteOnly ) )
-		kWarning() << "couldn't open /dev/dsp for writing" << endl;
-	else
-	{
-		int fd = m_dsp.handle();
-		int format = AFMT_S16_LE;
-		int stereo = 1;
-		int samplingRate = 44100;
-		ioctl( fd, SNDCTL_DSP_SETFMT, &format );
-		ioctl( fd, SNDCTL_DSP_STEREO, &stereo );
-		ioctl( fd, SNDCTL_DSP_SPEED, &samplingRate );
-	}
-#endif*/
-}
-
-void AudioOutput::closeDevice()
-{
-	m_dsp.close();
 }
 
 }} //namespace Phonon::Xine
