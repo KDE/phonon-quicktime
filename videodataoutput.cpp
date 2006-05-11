7f271a918b39e795a42902098c12a16b5b065b6a
Matthias Kretz <kretz@kde.org>
2006-05-11 15:25:29 +0000

    these are needed as well

---
+++ videodataoutput.cpp
@@ -0,0 +1,64 @@
+/*  This file is part of the KDE project
+    Copyright (C) 2006 Matthias Kretz <kretz@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2 as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+
+*/
+
+#include "videodataoutput.h"
+#include <kdebug.h>
+
+namespace Phonon
+{
+namespace Xine
+{
+VideoDataOutput::VideoDataOutput( QObject* parent )
+	: QObject( parent )
+{
+}
+
+VideoDataOutput::~VideoDataOutput()
+{
+}
+
+Phonon::VideoDataOutput::Format VideoDataOutput::format() const
+{
+	return m_format;
+}
+
+int VideoDataOutput::displayLatency() const
+{
+	return m_latency;
+}
+
+int VideoDataOutput::frameRate() const
+{
+	return 25;
+}
+
+void VideoDataOutput::setFormat( Phonon::VideoDataOutput::Format format )
+{
+	m_format = format;
+}
+
+void VideoDataOutput::setDisplayLatency( int milliseconds )
+{
+	m_latency = milliseconds;
+}
+
+}} //namespace Phonon::Xine
+
+#include "videodataoutput.moc"
+// vim: sw=4 ts=4 noet
---
+++ videodataoutput.h
@@ -0,0 +1,66 @@
+/*  This file is part of the KDE project
+    Copyright (C) 2006 Matthias Kretz <kretz@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2 as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+
+*/
+#ifndef Phonon_XINE_VIDEODATAOUTPUT_H
+#define Phonon_XINE_VIDEODATAOUTPUT_H
+
+#include "abstractvideooutput.h"
+#include <phonon/ifaces/videodataoutput.h>
+#include <phonon/videoframe.h>
+#include <QVector>
+#include <QByteArray>
+#include <QObject>
+
+namespace Phonon
+{
+namespace Xine
+{
+	/**
+	 * \author Matthias Kretz <kretz@kde.org>
+	 */
+	class VideoDataOutput : public QObject, virtual public Ifaces::VideoDataOutput, public Phonon::Xine::AbstractVideoOutput
+	{
+		Q_OBJECT
+		public:
+			VideoDataOutput( QObject* parent );
+			~VideoDataOutput();
+
+			virtual Phonon::VideoDataOutput::Format format() const;
+			virtual int displayLatency() const;
+			virtual int frameRate() const;
+			virtual void setFormat( Phonon::VideoDataOutput::Format format );
+			virtual void setDisplayLatency( int milliseconds );
+
+			virtual void* internal1( void* = 0 ) { return static_cast<Phonon::Xine::AbstractVideoOutput*>( this ); }
+
+		signals:
+			void frameReady( const Phonon::VideoFrame& frame );
+			void endOfMedia();
+
+		public:
+			virtual QObject* qobject() { return this; }
+			virtual const QObject* qobject() const { return this; }
+
+		private:
+			Phonon::VideoDataOutput::Format m_format;
+			int m_latency;
+	};
+}} //namespace Phonon::Xine
+
+// vim: sw=4 ts=4 tw=80 noet
+#endif // Phonon_XINE_VIDEODATAOUTPUT_H
