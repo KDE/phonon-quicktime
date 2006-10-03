9497431735b1a2c4a159cd209823e7f50f95b9e1
Matthias Kretz <kretz@kde.org>
2006-10-03 21:24:47 +0000

    - no need to install the xine bytestream plugin anymore - xineOpen should work now even if endOfStream has already been called

--- xineplugins/bytestream/plugin.c
+++ xineplugins/bytestream/plugin.c
@@ -8,7 +8,7 @@ extern void *init_plugin (xine_t *xine, void *data);
 /*
  * exported plugin catalog entry
  */
-const plugin_info_t xine_plugin_info[] = {
+const plugin_info_t kbytestream_xine_plugin_info[] = {
     /* type, API, "name", version, special_info, init_function */
     { PLUGIN_INPUT, 17, "KBYTESTREAM", XINE_VERSION_CODE, NULL, init_plugin },
     { PLUGIN_NONE, 0, "", 0, NULL, NULL }
