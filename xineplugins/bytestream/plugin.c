2b7980239785e75c2405ac551eb9c232afc39a2a
Matthias Kretz <kretz@kde.org>
2006-10-03 10:40:56 +0000

    A xine input plugin to pass data from the application to xine: needed for Xine::ByteStream

---
+++ xineplugins/bytestream/plugin.c
@@ -0,0 +1,15 @@
+#include <xine.h>
+#include <xine/compat.h>
+#include <xine/input_plugin.h>
+#include <xine/xine_internal.h>
+#include <xine/xineutils.h>
+
+extern void *init_plugin (xine_t *xine, void *data);
+/*
+ * exported plugin catalog entry
+ */
+const plugin_info_t xine_plugin_info[] = {
+    /* type, API, "name", version, special_info, init_function */
+    { PLUGIN_INPUT, 17, "KBYTESTREAM", XINE_VERSION_CODE, NULL, init_plugin },
+    { PLUGIN_NONE, 0, "", 0, NULL, NULL }
+};
