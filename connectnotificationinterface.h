3403915b75711a88c52982c07c95ea9b788c54ae
Matthias Kretz <kretz@kde.org>
2007-09-29 13:37:33 +0000

    when the connection graph has changed let AudioOutput know so that it can send an upstreamEvent to notify the xine_stream_t of the volume

--- connectnotificationinterface.h
+++ connectnotificationinterface.h
@@ -1,10 +1,9 @@
 /*  This file is part of the KDE project
     Copyright (C) 2007 Matthias Kretz <kretz@kde.org>
 
-    This program is free software; you can redistribute it and/or
+    This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+    License version 2 as published by the Free Software Foundation.
 
     This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -30,9 +29,6 @@ class ConnectNotificationInterface
 {
     public:
         virtual ~ConnectNotificationInterface() {}
-        /**
-         * This function is called before the xine thread is issued the rewire commands
-         */
         virtual void graphChanged() = 0;
 };
 
